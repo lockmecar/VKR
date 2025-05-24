be2435817a651a1b80774ae07b9223efa37103f2
lockmecar <lockmecar@gmail.com>
2025-05-24 19:27:18 +0300

    Создан класс MD5Hasher.

---
+++ PaymentDefender/Utilites/MD5Hasher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentDefender
+{
+    public static class MD5Hasher
+    {
+        public static string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
