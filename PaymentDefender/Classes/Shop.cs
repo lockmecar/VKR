f88edefd56396a435e63f75a3e05b3cf08668028
lockmecar <lockmecar@gmail.com>
2025-05-31 17:24:16 +0300

    Создал класс Shop

---
+++ PaymentDefender/Classes/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentDefender.Classes
+{
+    public class Shop
+    {
+        public Shop(string name, int popularity)
+        {
+            Name = name;
+            Popularity = popularity;
+        }
+        public string Name {  get; set; }
+        public int Popularity { get; set; }
+    }
+}
