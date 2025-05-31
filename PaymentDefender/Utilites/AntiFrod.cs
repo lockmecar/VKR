a007e608eb31d294775c5873f3b108e71aa63103
lockmecar <lockmecar@gmail.com>
2025-05-31 18:45:08 +0300

    Убраны лишние параметры для антифрода

--- PaymentDefender/Utilites/AntiFrod.cs
+++ PaymentDefender/Utilites/AntiFrod.cs
@@ -9,13 +9,13 @@ namespace PaymentDefender
 { 
     public static class AntiFrod
     {
-        public static TransactionLevel CheckPay(float moneyCount, string login, string password, string cardNumber, DateTime date, string cvv,
-            string countryIp, float freqOfPay, string device, Shop purposeOfPayment, User user)
+        public static TransactionLevel CheckPay(float moneyCount, string countryIp, float freqOfPay, string device, Shop purposeOfPayment, User user)
         {
             int warnings = 0;
             if (moneyCount > 30000) warnings += 2; // Проверка на большую сумму перевода
             if (countryIp != user.CountryLiving) warnings += 2; // Проверка айпи с которого запрашивают оплату
-            if ((freqOfPay > user.FreqOfPay * 1.5)) warnings += 2; // Проверка переодичности платежей
+            if (freqOfPay > user.FreqOfPay * 1.5) warnings += 2; // Проверка переодичности платежей
+            if (freqOfPay < user.FreqOfPay / 1.5) warnings++;
             if (device != user.LastDevice) warnings++; // Проверка устройства
             if (purposeOfPayment.Popularity < 7) warnings++;
             if (purposeOfPayment.Popularity < 4) warnings++; // Проверка условной известности куда назначен платеж
--- PaymentDefender/Utilites/PaymentGateway.cs
+++ PaymentDefender/Utilites/PaymentGateway.cs
@@ -12,7 +12,7 @@ namespace PaymentDefender
         public static void Pay(float moneyCount, string login, string password, string cardNumber, DateTime date, string cvv, string countryIp,float freqOfPay, string device, Shop purposeOfPayment)
         {
             User user = Bank.GetUserByLogin(login);
-            TransactionLevel transactionLevel = AntiFrod.CheckPay(moneyCount, login, password, cardNumber, date, cvv, countryIp, freqOfPay, device, purposeOfPayment, user); // Тут вызывается антифрод, который возвращает вердикт
+            TransactionLevel transactionLevel = AntiFrod.CheckPay(moneyCount, countryIp, freqOfPay, device, purposeOfPayment, user); // Тут вызывается антифрод, который возвращает вердикт
             if (transactionLevel == TransactionLevel.Green)
             {
                 Bank.AddSession(Authentificator.Authentificate(login, password), login);
