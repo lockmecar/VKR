4089635a660353ebd011214aa5c17dd49dbdd8f8
Игорь <lockmecar@gmail.com>
2025-06-03 15:09:38 +0300

    Добавлено автоматическое определение местоположения папки Source в проекте

--- PaymentDefender/Utilites/Authentificator.cs
+++ PaymentDefender/Utilites/Authentificator.cs
@@ -10,8 +10,25 @@ namespace PaymentDefender
     {
         public static Session Authentificate(string login, string password) 
         {
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string exeDirectory = Path.GetDirectoryName(exePath);
 
-            string filePath = "Z:\\projects\\learning\\VKR\\PaymentDefender\\Sources\\" + login + ".json";
+            // Поднимаемся вверх по директориям, пока не найдем папку Sources
+            DirectoryInfo directory = new DirectoryInfo(exeDirectory);
+            while (directory != null && !directory.GetDirectories("Sources").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            if (directory == null)
+            {
+                Console.WriteLine("Не удалось найти папку Sources");
+                return null;
+            }
+
+            string sourcesPath = Path.Combine(directory.FullName, "Sources");
+            string filePath = Path.Combine(sourcesPath, login + ".json");
+
             if (File.Exists(filePath))
             {
                 User buf = JsonFileManager.ReadObject<User>(login);
--- PaymentDefender/Utilites/JsonFileManager.cs
+++ PaymentDefender/Utilites/JsonFileManager.cs
@@ -6,9 +6,22 @@ namespace PaymentDefender
 {
     public static class JsonFileManager
     {
+        static JsonFileManager()
+        {
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string exeDirectory = Path.GetDirectoryName(exePath);
+            DirectoryInfo directory = new DirectoryInfo(exeDirectory);
+            while (directory != null && !directory.GetDirectories("Sources").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            SourcePath = Path.Combine(directory.FullName, "Sources");
+        }
+        public static string SourcePath {  get; set; }
         public static void WriteObject<T>(T obj) where T : class // сериализация объекта класса в файл по заданному пути
         {
-            string filePath = "D:\\dev\\VKR\\PaymentDefender\\Sources\\" + obj.ToString() + ".json";
+            string filePath = $"{SourcePath}{obj.ToString()}.json";
             if (!Directory.Exists(Path.GetDirectoryName(filePath)))
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(filePath));
@@ -20,7 +33,7 @@ namespace PaymentDefender
 
         public static T ReadObject<T>(string emailLogin) where T : class  // десериализация объекта класса из файла по заданному пути
         {
-            string filePath = "D:\\dev\\VKR\\PaymentDefender\\Sources\\" + emailLogin + ".json";
+            string filePath = $"{SourcePath}{emailLogin}.json";
             if (!File.Exists(filePath))
             {
                 throw new FileNotFoundException("Файл не найден по указанному пути.", filePath);
